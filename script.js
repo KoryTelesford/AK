9691292be7471f3fac6c802fa2f108dfdade799a
Kory Telesford <tkory85@gmail.com>
2019-05-15 21:42:31 +0000

    Stage 2

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 /* global $ */
+
+$("button").click(function() {
+    
+
 $.ajax({
     url: " https://api.soundcloud.com/tracks?q=beyonce&client_id=5aa8e389ba4e24b6106af5159ab3e344",
     method: "GET",
     success: function(response){
         console.log(response);
-    },
+        $("searchButton").click(function() {
+          var searchTerm = $("#searchTerm").val();
+          var giphyUrl =" https://api.soundcloud.com/tracks?q=beyonce&client_id=5aa8e389ba4e24b6106af5159ab3e344";
+            
+            $.ajax({
+             url: giphyUrl,
+             method: "GET",
+             success: function(response) {
+                  var imageSrc =response[1].user.permalink_url;
+                 console.log(response);
+                 
+                 var imageHtml= "<imgsrc= " + imageSrc + "/>"
+                 
+                 
+             }}
+                
+                
+                
+                
+            );
+            
+            
+        }); 
+    }
+    
 });
